0f5598d300bab2cbece142a1ea5cee36be91325a
Luis Camacho III <luiscamachoiii@gmail.com>
2024-11-19 15:06:55 -0600

    update avatar

--- home/src/components/MainAvatar.tsx
+++ home/src/components/MainAvatar.tsx
@@ -3,7 +3,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 
-import { Box, Link, Stack, Typography } from '@mui/material';
+import { Box, Grid2 as Grid, Link, Stack, Typography } from '@mui/material';
 import { Link as RouteLink } from 'react-router-dom';
 import { IconButtonLink } from '../shared/IconButtonLink';
 import { Row } from '../shared/Row';
@@ -15,24 +15,32 @@ export const MainAvatar = () => {
         </RouteLink>
         <Typography variant="h1" fontWeight={500}>Luis Camacho III</Typography>
         <Typography >Your friendly, neighborhood, software engineer!</Typography>
-        <Row justifyContent='space-between'>
-            <Row>
-                <IconButtonLink href="https://www.facebook.com/LCamachoIII">
-                    <FacebookIcon sx={{ fill: '#1877F2' }} />
-                </IconButtonLink>
-                <IconButtonLink href="https://www.linkedin.com/in/luiscamachoiii/">
-                    <LinkedInIcon sx={{ fill: '#0a66c2' }} />
-                </IconButtonLink>
-                <IconButtonLink href="https://github.com/lciii">
-                    <GitHubIcon sx={{ fill: 'black' }} />
-                </IconButtonLink>
-                <IconButtonLink href="https://youtube.com/playlist?list=PL75NYWDJQHD_8eqxU_tx7eozlIQ0hWM4M&si=DWEg-BobiKQ2VnDJ">
-                    <YouTubeIcon sx={{ fill: '#FF0000' }} />
-                </IconButtonLink>
-            </Row>
+        <Grid container justifyContent='space-between'>
+            <Grid container>
+                <Grid>
+                    <IconButtonLink href="https://www.facebook.com/LCamachoIII">
+                        <FacebookIcon sx={{ fill: '#1877F2' }} />
+                    </IconButtonLink>
+                </Grid>
+                <Grid>
+                    <IconButtonLink href="https://www.linkedin.com/in/luiscamachoiii/">
+                        <LinkedInIcon sx={{ fill: '#0a66c2' }} />
+                    </IconButtonLink>
+                </Grid>
+                <Grid>
+                    <IconButtonLink href="https://github.com/lciii">
+                        <GitHubIcon sx={{ fill: 'black' }} />
+                    </IconButtonLink>
+                </Grid>
+                <Grid>
+                    <IconButtonLink href="https://youtube.com/playlist?list=PL75NYWDJQHD_8eqxU_tx7eozlIQ0hWM4M&si=DWEg-BobiKQ2VnDJ">
+                        <YouTubeIcon sx={{ fill: '#FF0000' }} />
+                    </IconButtonLink>
+                </Grid>
+            </Grid>
             <Link href='mailto:luiscamachoiii@gmail.com' underline='none'>
                 <Typography >luiscamachoiii@gmail.com</Typography>
             </Link>
-        </Row>
+        </Grid>
     </Stack>
 }
--- home/src/shared/IconButtonLink.tsx
+++ home/src/shared/IconButtonLink.tsx
@@ -5,10 +5,15 @@ import React from 'react';
 const iconSize = '1em'
 
 export const IconButtonLink = (props: IconButtonProps & React.ComponentProps<'a'>) => {
-    return <IconButton target="_blank" rel="noopener noreferrer" {...props} sx={{
+    return <IconButton target="_blank" rel="noopener noreferrer" {...props} sx={t => ({
         padding: '4px',
         '& svg': {
-            height: iconSize, width: iconSize
+            height: '2em',
+            width: '2em',
+            [t.breakpoints.up('md')]: {
+                height: '1em',
+                width: '1em',
+            },
         }
-    }} />
+    })} />
 }
